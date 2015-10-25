fc63b0a93e8860a93806bbee42965f210f0e3c40
ranjukr <ranjukr@INranjukr.local>
2015-10-25 12:55:09 +0530

    Needed files created

---
+++ crossRiverTest.js
@@ -0,0 +1,26 @@
+var lib = require('./crossRiver.js').lib;
+var chai = require('chai');
+var assert = chai.assert;
+
+var test = {};
+exports.test = test;
+
+// test["moveRight moves the selected items from left to right for two item selected"] = function(){
+// 	var actual = lib.moveToRight('child','ghost');
+
+// 	assert.deepEqual(2,actual.left.child);
+// 	assert.deepEqual(2,actual.left.ghost)
+// 	assert.deepEqual(1,actual.right.child)
+// 	assert.deepEqual(1,actual.right.ghost)
+// };
+
+// test["moveRight moves the selected items from left to right for one item selected"] = function(){
+// 	var actual = lib.moveToRight('child');
+
+// 	assert.deepEqual(2,actual.left.child);
+// 	assert.deepEqual(3,actual.left.ghost)
+// 	assert.deepEqual(1,actual.right.child)
+// 	assert.deepEqual(0,actual.right.ghost)
+
+// };
+
---
+++ main.js
@@ -0,0 +1,15 @@
+var lib = require('./crossRiver.js').lib;
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var initial = lib.initial();
+var side = process.argv[2];
+var move1 = process.argv[3];
+var move2 = process.argv[4];
+
+var a = lib[side](move1,move2,initial)
+console.log(a)
